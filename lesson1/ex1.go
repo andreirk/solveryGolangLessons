347fdd39fe609b796fff151680a576c830cac052
Andrey Sokolov <falkonirk@gmail.com>
2023-11-21 07:22:35 +0100

    add flags, and bit to change

--- lesson1/ex1.go
+++ lesson1/ex1.go
@@ -1,51 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
+	_ "os"
 	"strconv"
 )
 
 // Дана переменная int64. Разработать программу которая устанавливает i-й бит в 1 или 0.
 // $ ./program x 92323232323345 i 2
 
-func setIthBit(number, i int) int {
-	bitMask := 1 << i
-	number = number | bitMask
-	return number
+func setIthBit(number, i, theBit int) int {
+	var newNumber, bitMask int
+	bitMask = 1 << i
+	if theBit == 0 {
+		newNumber = number & ^bitMask
+	} else {
+		newNumber = number | bitMask
+	}
+
+	return newNumber
 }
 
 func main() {
-	argsWithoutProg := os.Args[1:]
-	theNum := argsWithoutProg[1]
-	num64, _ := strconv.Atoi(theNum)
-	numOfIdx, _ := strconv.Atoi(argsWithoutProg[3])
 
-	result := setIthBit(num64, numOfIdx)
-	fmt.Println(result) // Output: 14 (Binary: 1110)
-	//fmt.Println("Was: ", strconv.FormatInt(int64(num64), 2))
-	fmt.Printf("Was: %s Become: %s \n", strconv.FormatInt(int64(num64), 2), strconv.FormatInt(int64(result), 2))
+	numPtr := flag.Int("num", 0, "Number")
+	idxPtr := flag.Int("i", 0, "Index")
+	toBitPtr := flag.Int("toBit", 0, "To bit")
+
+	flag.Parse()
+	numOfIdx := *idxPtr
+	theNum := *numPtr
+	toBit := *toBitPtr
+	result := setIthBit(theNum, numOfIdx, toBit)
+
+	fmt.Println("num", theNum)
+	fmt.Println("idx", numOfIdx)
+	fmt.Println("Result num", result)
+	fmt.Printf("Was: %s Become: %s \n", strconv.FormatInt(int64(theNum), 2), strconv.FormatInt(int64(result), 2))
 }
-
-// 	argsWithoutProg := os.Args[1:]
-
-// 	theNum := argsWithoutProg[1]
-// 	numOfIdx, _ := strconv.Atoi(argsWithoutProg[3])
-// 	num64, _ := strconv.Atoi(theNum)
-// 	_ = numOfIdx
-// 	valOfBit := (num64 & (1 << (numOfIdx - 1))) >> (numOfIdx - 1)
-// 	mask := 1 << numOfIdx
-// 	temp := uint32(num64) & bits.Reverse32(uint32(mask))
-
-// 	theVal := (1 << numOfIdx) | num64
-
-// 	fmt.Println("Temp", temp)
-// 	fmt.Println("Binary num: ", strconv.FormatInt(int64(num64), 2))
-// 	fmt.Println("Bit: ", valOfBit)
-// 	fmt.Println("Here: ", strconv.FormatInt(int64(theVal), 2))
-
-// 	//fmt.Prinum64tln(argsWithoutProg)
-
-// func reverse(x uint32, size uint32) uint32 {
-// 	return bits.Reverse32(x) >> (32 - size)
-// }

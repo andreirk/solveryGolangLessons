df7e42a4df0795f49dbc0ddfc31ebb8d90747fb2
Andrey Sokolov <falkonirk@gmail.com>
2023-11-21 08:13:01 +0100

    ex3 add flags and struct

--- lesson1/ex3.go
+++ lesson1/ex3.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"log"
 	_ "math"
@@ -17,9 +18,19 @@ import (
 	_ "strconv"
 )
 
-type Currency struct {
-	name      string
-	rateToUsd float64
+type Currency string
+
+var currencyMap map[string]float64
+
+type Money struct {
+	amount   float64
+	currency Currency
+}
+
+func (m *Money) add(m2 Money) float64 {
+	result1 := m.amount * currencyMap[string(m.currency)]
+	result2 := m2.amount * currencyMap[string(m2.currency)]
+	return result1 + result2
 }
 
 /*
@@ -31,27 +42,22 @@ Usage example:
 *
 */
 func main() {
-
 	// 1. parse file
 	// 2. make map string[float64]
-	argsWithoutProg := os.Args[0:]
-	fileName := argsWithoutProg[1]
-	amount, _ := strconv.ParseFloat(argsWithoutProg[2], 64)
-	from := argsWithoutProg[3]
-	to := argsWithoutProg[4]
+	fileName := flag.String("file", "", "File name")
 
-	// readCsv2(fileName)
+	amount := *flag.Float64("amount", 0, "Amount")
+	from := *flag.String("from", "", "From")
+	to := *flag.String("to", "", "TO")
 
 	currencyMap := make(map[string]float64)
-	records := readCsvFile(fileName)
+	records := readCsvFile(*fileName)
 
 	for _, record := range records {
 		cur := record[0]
 		val, _ := strconv.ParseFloat(record[1], 64)
-		// fmt.Printf("%d %s - %s", idx+1, record[0], record[1])
 		currencyMap[cur] = val
 	}
-	// fmt.Println(currencyMap)
 
 	result := amount * currencyMap[from] * currencyMap[to]
 	fmt.Printf("Result of conversion is: %2.3f \n", result)
@@ -66,46 +72,13 @@ func readCsvFile(filePath string) [][]string {
 	defer f.Close()
 
 	csvReader := csv.NewReader(f)
+	// csvReader.LazyQuotes = true
+	// csvReader.FieldsPerRecord = -1
+	// csvReader.Comma = ','
 	records, err := csvReader.ReadAll()
 	if err != nil {
 		log.Fatal("Unable to parse file as CSV for "+filePath, err)
 	}
 
 	return records
-}
-
-func readCsv2(fileName string) {
-	file, err := os.Open(fileName)
-
-	if err != nil {
-		log.Fatal("Error while reading the file", err)
-	}
-
-	defer file.Close()
-
-	reader := csv.NewReader(file)
-	reader.LazyQuotes = true
-	reader.FieldsPerRecord = -1
-	reader.Comma = ','
-
-	// for range []int{1, 2} {
-	// 	row, err := reader.Read()
-	// 	if err == io.EOF {
-	// 		break
-	// 	}
-	// 	if err != nil {
-	// 		log.Fatal("-- here is err: ", err)
-	// 	}
-	// 	fmt.Println("--Here", row[1])
-	// 	// fmt.Println(row)
-	// }
-
-	records, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Error reading records")
-	}
-
-	for _, eachrecord := range records {
-		fmt.Printf("Currency: %s = %s \n", eachrecord[0], eachrecord[1])
-	}
 }
